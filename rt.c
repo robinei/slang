8a8379c91a7b9a437392dca93661d842be253b93
Robin Eidissen <robinei@gmail.com>
2017-06-04 19:58:21 +0200

    move to fat ptr approach instead of storing type in box

--- rt.c
+++ rt.c
@@ -28,11 +28,14 @@ enum { FALSE, TRUE };
 
 
 enum rt_kind {
+    /* gc-managed heap object. contains the boxed value after its header */
+    RT_KIND_BOX,
+    
     /* reference kinds */
     RT_KIND_PTR,
-    RT_KIND_BOX_PTR, /* always points to struct rt_box */
-    RT_KIND_INTERIOR_BOX_PTR, /* points inside a struct rt_box, offset by box_offset */
-
+    RT_KIND_FAT_PTR,
+    RT_KIND_BOX_PTR, /* points inside a box, offset by box_offset */
+    
     /* composite kinds */
     RT_KIND_STRUCT,
     RT_KIND_ARRAY,
@@ -52,16 +55,17 @@ struct rt_type {
        in that case in needs to be boxed. */
     u32 size;
 
-    /* ptr and array specific */
+    /* for RT_KIND_BOX, RT_KIND_PTR, RT_KIND_BOX_PTR, RT_KIND_ARRAY.
+       the "contained" or "pointed to" type */
     struct rt_type *target_type;
 
-    /* interior ref specific. stores offset from the pointer this type belongs to,
-       to the start of the enclosing box */
-    i32 box_offset;
-
-    /* struct specific */
+    /* for RT_KIND_STRUCT */
     u32 field_count;
     struct rt_struct_field *fields;
+
+    /* for RT_KIND_BOX_PTR */
+    struct rt_type *box_type; /* type of enclosing box */
+    u32 box_offset; /* how many bytes from start of enclosing box the pointed to value begins */
 };
 
 struct rt_struct_field {
@@ -70,9 +74,15 @@ struct rt_struct_field {
     u32 offset;
 };
 
+struct rt_fatptr {
+    void *ptr;
+    struct rt_type *type;
+};
+
 struct rt_box {
-    intptr_t type_ptr; /* bit 0 is the GC mark bit */
-    struct rt_box *next; /* used to chain all allocated boxes so the GC can run a sweep */
+    /* pointer used to chain all allocated boxes so the GC can run a sweep.
+       bit 0 is the GC mark bit, so the box pointers must be 2-byte aligned */
+    intptr_t header;
 
     /* the actual data for the boxed value will follow after the box header */
 };
@@ -93,22 +103,25 @@ struct rt_thread_ctx {
 
 
 
-#define rt_box_get_type(box) ((struct rt_type *)((box)->type_ptr & ~(intptr_t)1))
-#define rt_box_is_marked(box) ((box)->type_ptr & 1)
-#define rt_box_set_mark(box) do { (box)->type_ptr |= 1; } while(0)
-#define rt_box_clear_mark(box) do { (box)->type_ptr &= ~(intptr_t)1; } while(0)
+#define rt_boxheader_get_next(h) ((struct rt_box *)((h) & ~(intptr_t)1))
+#define rt_boxheader_set_next(h, next) do { (h) = (intptr_t)(next) | ((h) & (intptr_t)1); } while(0)
+#define rt_boxheader_is_marked(h) ((h) & 1)
+#define rt_boxheader_set_mark(h) do { (h) |= 1; } while(0)
+#define rt_boxheader_clear_mark(h) do { (h) &= ~(intptr_t)1; } while(0)
 
 
 
 
 #define RT_DEF_TYPE(name, kind, size)                                   \
     struct rt_type rt_type_##name = { kind, size, };                    \
+    struct rt_type rt_type_box_##name = { RT_KIND_BOX, 0, &rt_type_##name }; \
     struct rt_type rt_type_ptr_##name = { RT_KIND_PTR, sizeof(void *), &rt_type_##name }; \
-    struct rt_type rt_type_array_##name = { RT_KIND_ARRAY, 0, &rt_type_##name }
+    struct rt_type rt_type_array_##name = { RT_KIND_ARRAY, 0, &rt_type_##name }; \
+    struct rt_type rt_type_box_array_##name = { RT_KIND_BOX, 0, &rt_type_array_##name };
 
 #define RT_DEF_SCALAR(name, kind) RT_DEF_TYPE(name, kind, sizeof(name))
 
-RT_DEF_TYPE(box_ptr, RT_KIND_BOX_PTR, sizeof(struct rt_box *));
+RT_DEF_TYPE(fatptr, RT_KIND_FAT_PTR, sizeof(struct rt_fatptr));
 
 RT_DEF_SCALAR(b8, RT_KIND_BOOL);
 RT_DEF_SCALAR(b32, RT_KIND_BOOL);
@@ -127,26 +140,20 @@ RT_DEF_SCALAR(f32, RT_KIND_REAL);
 RT_DEF_SCALAR(f64, RT_KIND_REAL);
 
 
-struct rt_box rt_true = { (intptr_t)&rt_type_b32, };
-struct rt_box rt_false = { (intptr_t)&rt_type_b32, };
+struct rt_box rt_true = {0};
+struct rt_box rt_false = {0};
 
 
 
 
 static void rt_gc_mark_single(char *ptr, struct rt_type *type);
 
-static void rt_gc_mark_box(struct rt_box *box) {
-    if (!box) {
+static void rt_gc_mark_box(struct rt_box *box, struct rt_type *boxed_type) {
+    if (!box || rt_boxheader_is_marked(box->header)) {
         return;
     }
-    if (rt_box_is_marked(box)) {
-        return;
-    }
-
-    rt_box_set_mark(box);
-
-    struct rt_type *type = rt_box_get_type(box);
-    rt_gc_mark_single((char *)box + sizeof(struct rt_box), type);
+    rt_boxheader_set_mark(box->header);
+    rt_gc_mark_single((char *)box + sizeof(struct rt_box), boxed_type);
 }
 
 static void rt_gc_mark_struct(char *ptr, struct rt_type *type) {
@@ -159,7 +166,19 @@ static void rt_gc_mark_struct(char *ptr, struct rt_type *type) {
     }
 }
 
+static b32 rt_gc_type_needs_scan(struct rt_type *type) {
+    switch (type->kind) {
+    case RT_KIND_BOOL:
+    case RT_KIND_SIGNED:
+    case RT_KIND_UNSIGNED:
+    case RT_KIND_REAL:
+        return FALSE;
+    }
+    return TRUE;
+}
+
 static void rt_gc_mark_array(char *ptr, struct rt_type *type) {
+    struct rt_fatptr *f;
     struct rt_type *elem_type = type->target_type;
     u32 elem_size = elem_type->size;
     
@@ -174,22 +193,32 @@ static void rt_gc_mark_array(char *ptr, struct rt_type *type) {
     
     switch (elem_type->kind) {
     case RT_KIND_PTR:
+        if (!rt_gc_type_needs_scan(elem_type->target_type)) {
+            break;
+        }
         for (u32 i = 0; i < length; ++i) {
             rt_gc_mark_single(*(char **)(ptr + i*elem_size), elem_type->target_type);
         }
         break;
-    case RT_KIND_BOX_PTR:
+    case RT_KIND_FAT_PTR:
         for (u32 i = 0; i < length; ++i) {
-            rt_gc_mark_box(*(struct rt_box **)(ptr + i*elem_size));
+            f = (struct rt_fatptr *)(ptr + i*elem_size);
+            if (f->ptr) {
+                rt_gc_mark_single(f->ptr, f->type);
+            }
         }
         break;
     case RT_KIND_STRUCT:
+        assert(elem_type->size);
         for (u32 i = 0; i < length; ++i) {
             rt_gc_mark_struct(ptr + i*elem_size, elem_type);
         }
         break;
     case RT_KIND_ARRAY:
         assert(elem_type->size);
+        if (!rt_gc_type_needs_scan(elem_type->target_type)) {
+            break;
+        }
         for (u32 i = 0; i < length; ++i) {
             rt_gc_mark_array(ptr + i*elem_size, elem_type);
         }
@@ -198,15 +227,22 @@ static void rt_gc_mark_array(char *ptr, struct rt_type *type) {
 }
 
 static void rt_gc_mark_single(char *ptr, struct rt_type *type) {
+    struct rt_fatptr *f;
     switch (type->kind) {
+    case RT_KIND_BOX:
+        rt_gc_mark_box((struct rt_box *)ptr, type->target_type);
+        break;
     case RT_KIND_PTR:
         rt_gc_mark_single(*(char **)ptr, type->target_type);
         break;
-    case RT_KIND_BOX_PTR:
-        rt_gc_mark_box(*(struct rt_box **)ptr);
+    case RT_KIND_FAT_PTR:
+        f = (struct rt_fatptr *)ptr;
+        if (f->ptr) {
+            rt_gc_mark_single(f->ptr, f->type);
+        }
         break;
-    case RT_KIND_INTERIOR_BOX_PTR:
-        rt_gc_mark_box((struct rt_box *)(ptr + type->box_offset));
+    case RT_KIND_BOX_PTR:
+        rt_gc_mark_box((struct rt_box *)(ptr - type->box_offset), type->box_type);
         break;
     case RT_KIND_STRUCT:
         rt_gc_mark_struct(ptr, type);
@@ -234,19 +270,19 @@ void rt_gc_run(struct rt_thread_ctx *ctx) {
     }
 
     /* sweep */
-    struct rt_box **slot = &ctx->boxes;
+    intptr_t *slot = (intptr_t *)&ctx->boxes;
     struct rt_box *unreachable = NULL;
     while (1) {
-        struct rt_box *box = *slot;
+        struct rt_box *box = rt_boxheader_get_next(*slot);
         if (!box) {
             break;
         }
-        if (rt_box_is_marked(box)) {
-            rt_box_clear_mark(box);
-            slot = &box->next;
+        if (rt_boxheader_is_marked(box->header)) {
+            rt_boxheader_clear_mark(box->header);
+            slot = &box->header;
         } else {
-            *slot = box->next;
-            box->next = unreachable;
+            rt_boxheader_set_next(*slot, rt_boxheader_get_next(box->header));
+            rt_boxheader_set_next(box->header, unreachable);
             unreachable = box;
         }
     }
@@ -254,67 +290,38 @@ void rt_gc_run(struct rt_thread_ctx *ctx) {
     /* free unreachable boxes. could be done on another thread */
     while (unreachable) {
         struct rt_box *box = unreachable;
-        unreachable = box->next;
+        unreachable = rt_boxheader_get_next(box->header);
         printf("freeing 1 object\n");
         free(box);
     }
 }
 
-struct rt_box *rt_alloc_box(struct rt_thread_ctx *ctx, u32 size, struct rt_type *type) {
+struct rt_box *rt_alloc_box(struct rt_thread_ctx *ctx, u32 size) {
     struct rt_box *box = (struct rt_box *)calloc(1, sizeof(struct rt_box) + size);
-    box->type_ptr = (intptr_t)type;
-    box->next = ctx->boxes;
+    rt_boxheader_set_next(box->header, ctx->boxes);
     ctx->boxes = box;
     return box;
 }
-
-
-static b32 rt_math_is_signed_int(struct rt_box *x) {
-    return FALSE;
-}
-
-static b32 rt_math_is_real(struct rt_box *x) {
-}
-
-static b32 rt_math_is_integral(struct rt_box *x) {
-}
-
-static f64 rt_math_to_f64(struct rt_box *x) {
-}
-
-struct rt_box *rt_math_plus(struct rt_box *x, struct rt_box *y) {
-    if (rt_math_is_integral(x)) {
-        if (rt_math_is_integral(y)) {
-            
-        }
-        if (rt_math_is_real(x)) {
-            f64 result = rt_math_to_f64(x) + rt_math_to_f64(y);
-        }    
-    }
-    if (rt_math_is_real(x)) {
-        f64 result = rt_math_to_f64(x) + rt_math_to_f64(y);
-    }
-}
-
 
 
 
 
 
 struct rt_cons {
-    struct rt_box *car;
-    struct rt_box *cdr;
+    struct rt_fatptr car;
+    struct rt_fatptr cdr;
 };
 
 struct rt_struct_field rt_struct_fields_cons[2] = {
-    { &rt_type_box_ptr, "car", offsetof(struct rt_cons, car) },
-    { &rt_type_box_ptr, "cdr", offsetof(struct rt_cons, cdr) },
+    { &rt_type_fatptr, "car", offsetof(struct rt_cons, car) },
+    { &rt_type_fatptr, "cdr", offsetof(struct rt_cons, cdr) },
 };
 
-struct rt_type rt_type_cons = { RT_KIND_STRUCT, sizeof(struct rt_cons), NULL, 0, 2, rt_struct_fields_cons };
+struct rt_type rt_type_cons = { RT_KIND_STRUCT, sizeof(struct rt_cons), NULL, 2, rt_struct_fields_cons };
+struct rt_type rt_type_box_cons = { RT_KIND_BOX, 0, &rt_type_cons };
 
-struct rt_box *rt_new_cons(struct rt_thread_ctx *ctx, struct rt_box *car, struct rt_box *cdr) {
-    struct rt_box *box = rt_alloc_box(ctx, sizeof(struct rt_cons), &rt_type_cons);
+struct rt_box *rt_new_cons(struct rt_thread_ctx *ctx, struct rt_fatptr car, struct rt_fatptr cdr) {
+    struct rt_box *box = rt_alloc_box(ctx, sizeof(struct rt_cons));
     struct rt_cons *cons = (struct rt_cons *)((char *)box + sizeof(struct rt_box));
     cons->car = car;
     cons->cdr = cdr;
@@ -326,7 +333,7 @@ struct rt_box *rt_new_array(struct rt_thread_ctx *ctx, u32 length, struct rt_typ
     assert(!type->size); /* boxed arrays are unsized (have length stored in box, not type) */
     u32 elem_size = type->target_type->size;
     assert(elem_size);
-    struct rt_box *box = rt_alloc_box(ctx, sizeof(u32) + length*elem_size, type);
+    struct rt_box *box = rt_alloc_box(ctx, sizeof(u32) + length*elem_size);
     *(u32 *)((char *)box + sizeof(struct rt_box)) = length;
     return box;
 }
@@ -336,7 +343,7 @@ struct rt_box *rt_new_b32(b32 value) {
 }
 
 struct rt_box *rt_new_u32(struct rt_thread_ctx *ctx, u32 value) {
-    struct rt_box *box = rt_alloc_box(ctx, sizeof(u32), &rt_type_u32);
+    struct rt_box *box = rt_alloc_box(ctx, sizeof(u32));
     *(u32 *)((char *)box + sizeof(struct rt_box)) = value;
     return box;
 }
@@ -348,11 +355,11 @@ struct rt_array_u8 {
 
 struct rt_string { struct rt_array_u8 chars; };
 struct rt_struct_field rt_string_fields[1] = {{ &rt_type_array_u8, "chars", 0 }};
-struct rt_type rt_type_string = { RT_KIND_STRUCT, 0, NULL, 0, 1, rt_string_fields };
+struct rt_type rt_type_string = { RT_KIND_STRUCT, 0, NULL, 1, rt_string_fields };
 
 struct rt_box *rt_new_string_from_cstring(struct rt_thread_ctx *ctx, const char *data) {
     u32 length = strlen(data);
-    struct rt_box *box = rt_alloc_box(ctx, sizeof(u32) + length + 1, &rt_type_string);
+    struct rt_box *box = rt_alloc_box(ctx, sizeof(u32) + length + 1);
     struct rt_string *string = (struct rt_string *)(box + 1);
     string->chars.length = length;
     memcpy(string->chars.data, data, length + 1);
@@ -362,43 +369,51 @@ struct rt_box *rt_new_string_from_cstring(struct rt_thread_ctx *ctx, const char 
 
 struct rt_symbol { struct rt_string string; };
 struct rt_struct_field rt_symbol_fields[1] = {{ &rt_type_string, "string", 0 }};
-struct rt_type rt_type_symbol = { RT_KIND_STRUCT, 0, NULL, 0, 1, rt_symbol_fields };
+struct rt_type rt_type_symbol = { RT_KIND_STRUCT, 0, NULL, 1, rt_symbol_fields };
 
+
+
+static struct rt_fatptr rt_make_fatptr(void *ptr, struct rt_type *type) {
+    struct rt_fatptr f = { ptr, type };
+    return f;
+}
+
+static struct rt_fatptr rt_fatptr_nil = { NULL, NULL };
 
 
 #define rt_box_array_ref(box, type, index) (((type *)((char *)(box) + sizeof(struct rt_box) + sizeof(u32)))[index])
 
-#define rt_car(box) (((struct rt_cons *)((box) + 1))->car)
-#define rt_cdr(box) (((struct rt_cons *)((box) + 1))->cdr)
+#define rt_car(box) (((struct rt_cons *)((char *)(box) + sizeof(struct rt_box)))->car)
+#define rt_cdr(box) (((struct rt_cons *)((char *)(box) + sizeof(struct rt_box)))->cdr)
 
 
 int main(int argc, char *argv[]) {
     struct rt_thread_ctx ctx = {0,};
 
-    struct rt_box *x = rt_new_u32(&ctx, 123);
-    struct rt_box *y = rt_new_u32(&ctx, 100);
-    struct rt_box *z = rt_new_u32(&ctx, 33);
+    struct rt_fatptr x = rt_make_fatptr(rt_new_u32(&ctx, 123), &rt_type_box_u32);
+    struct rt_fatptr y = rt_make_fatptr(rt_new_u32(&ctx, 100), &rt_type_box_u32);
+    struct rt_fatptr z = rt_make_fatptr(rt_new_u32(&ctx, 33), &rt_type_box_u32);
 
-    struct rt_box *arr = rt_new_array(&ctx, 10, &rt_type_array_box_ptr);
-    rt_box_array_ref(arr, struct rt_box *, 0) = z;
+    struct rt_fatptr arr = rt_make_fatptr(rt_new_array(&ctx, 10, &rt_type_array_fatptr), &rt_type_box_array_fatptr);
+    rt_box_array_ref(arr.ptr, struct rt_fatptr, 0) = z;
 
-    struct rt_box *cons = rt_new_cons(&ctx, y, z);
-    rt_box_array_ref(arr, struct rt_box *, 1) = cons;
+    struct rt_fatptr cons = rt_make_fatptr(rt_new_cons(&ctx, y, z), &rt_type_box_cons);
+    rt_box_array_ref(arr.ptr, struct rt_fatptr, 1) = cons;
 
-    struct rt_type *types[4] = { &rt_type_box_ptr, &rt_type_box_ptr, &rt_type_box_ptr, NULL };
+    struct rt_type *types[4] = { &rt_type_fatptr, &rt_type_fatptr, &rt_type_fatptr, NULL };
     void *roots[5] = { ctx.roots, types, &x, &y, &arr };
     ctx.roots = roots;
     rt_gc_run(&ctx);
 
     printf("-\n");
 
-    rt_box_array_ref(arr, struct rt_box *, 0) = NULL;
-    rt_cdr(cons) = NULL;
+    rt_box_array_ref(arr.ptr, struct rt_fatptr, 0) = rt_fatptr_nil;
+    rt_cdr(cons.ptr) = rt_fatptr_nil;
     rt_gc_run(&ctx);
 
     printf("-\n");
 
-    rt_box_array_ref(arr, struct rt_box *, 1) = NULL;
+    rt_box_array_ref(arr.ptr, struct rt_fatptr, 1) = rt_fatptr_nil;
     rt_gc_run(&ctx);
 
     printf("-\n");
@@ -406,3 +421,4 @@ int main(int argc, char *argv[]) {
     ctx.roots = roots[0];
     rt_gc_run(&ctx);
 }
+
